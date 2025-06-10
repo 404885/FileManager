f033a3f66f62f4d5f327f29f1c3927df5f8a6152
404885 <584108053@qq.com>
2025-06-10 21:39:06 +0800

    读取文件

--- src/utils/Dialog.ts
+++ src/utils/Dialog.ts
@@ -1,27 +1,14 @@
 // utils/Dialog.ts
-import { createApp, h} from 'vue'
+import { createApp, h } from 'vue'
 import ModalDialog from '../components/Dialog/ModalDialog.vue'
 import FileOpenDialog from "../components/Dialog/child/FileOpenDialog.vue";
-import FileAddDialog from "@/components/Dialog/child/FileAddDialog.vue";
-import SettingDialog from "../components/Dialog/child/SettingDialog.vue";
-import UserDialog from "../components/Dialog/child/UserDialog.vue";
 
-
-let DialogMap = {
+const DialogMap = {
     openFile: FileOpenDialog,
-    addFile: FileAddDialog,
-    setting: SettingDialog,
-    user: UserDialog,
+
 }
 
-
-interface DialogOptions {
-    type: keyof typeof DialogMap   // 要渲染的组件
-    props?: Record<string, any> | null  // 给该组件传的 props
-    modalProps?: Record<string, any> // 给 ModalDialog 自身传的 props（比如标题）
-}
-
-export function openDialog({ type, props = {}, modalProps = {} }: DialogOptions) {
+export function openDialog() {
     const container = document.createElement('div')
     document.body.appendChild(container)
 
@@ -29,15 +16,14 @@ export function openDialog({ type, props = {}, modalProps = {} }: DialogOptions)
         render() {
             return h(ModalDialog,
                 {
-                    ...modalProps, // 展开 modalProps 对象
                     onClose: () => {
-                        app.unmount()
-                        container.remove()
-                    }
-                },
+                    app.unmount()
+                    container.remove()
+                }
+            },
                 {
-                    default: () => h(DialogMap[type], props) // 渲染具体的子组件
-            })
+                    default: () => h(FileOpenDialog),
+                })
         }
     })
 
