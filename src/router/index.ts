9735facb6b84342e62b367cad14ddfaf2a581e64
404885 <584108053@qq.com>
2025-07-02 22:03:14 +0800

    树状图完善

--- src/router/index.ts
+++ src/router/index.ts
@@ -57,4 +57,35 @@ const routers = createRouter({
     ]
 })
 
+routers.beforeEach((to, _from, next) => {
+    // 假设你想让根路径 '/' 默认带上 workspace 和 folder 参数
+    if (to.path === '/space' && !to.query.workspace && !to.query.folder) {
+        let workspace = 1;
+        let folder = null;
+
+        const lastWorkspace = localStorage.getItem('lastWorkspace');
+        const lastFolder = localStorage.getItem('lastFolder');
+
+        console.log(lastWorkspace, lastFolder);
+        if (lastWorkspace !== undefined){
+            workspace = Number(lastWorkspace);
+        }
+        if (lastFolder !== undefined){
+            folder = Number(lastFolder);
+        }
+
+        console.log(workspace,folder)
+
+        next({
+            path: '/space',
+            query: {
+                workspace,
+                folder,
+            }
+        });
+    } else {
+        next();
+    }
+});
+
 export default routers;
